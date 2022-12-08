d184ac84d40c0a6d00756f78cfe0c27f019d67e9
cfigueiroa <cfigueiroa@gmail.com>
2022-12-08 15:21:25 -0300

    feat: finish MVP

--- src/pages/SuccessPage/index.jsx
+++ src/pages/SuccessPage/index.jsx
@@ -1,23 +1,41 @@
-import { Link, useLocation } from "react-router-dom";
-import { SuccessContainer } from "./styles";
+import { useLocation } from "react-router-dom";
+import { SuccessContainer, Headline, Campos, CamposBox, StyledLink } from "./styles";
 
 export default function SuccessPage() {
   const location = useLocation();
   const { cpf, date, hour, movie, nome, seats } = location.state;
   return (
     <SuccessContainer>
-      <h2>Pedido feito com sucesso!</h2>
-      <h3>Filme e sessão</h3>
-      <h4>{movie}</h4>
-      <h4>{date} {hour}</h4>
-      <h3>Ingressos</h3>
-      {seats.map((seat) => (<h4 key={seat}>Assento {seat}</h4>))}
-      <h3>Comprador</h3>
-      <h4>Nome: {nome}</h4>
-      <h4>CPF: {cpf}</h4>
-      <Link to={`/`}>
+      <Headline>
+        <h2>
+          Pedido feito
+          <br />
+          com sucesso!
+        </h2>
+      </Headline>
+      <Campos>
+        <CamposBox>
+          <h3>Filme e sessão</h3>
+          <h4>{movie}</h4>
+          <h4>
+            {date} {hour}
+          </h4>
+        </CamposBox>
+        <CamposBox>
+          <h3>Ingressos</h3>
+          {seats.map((seat) => (
+            <h4 key={seat}>Assento {seat}</h4>
+          ))}
+        </CamposBox>
+        <CamposBox>
+          <h3>Comprador</h3>
+          <h4>Nome: {nome}</h4>
+          <h4>CPF: {cpf}</h4>
+        </CamposBox>
+      </Campos>
+      <StyledLink to={`/`}>
         <button>Voltar para Home</button>
-      </Link>
+      </StyledLink>
     </SuccessContainer>
   );
 }
--- src/pages/SuccessPage/styles.jsx
+++ src/pages/SuccessPage/styles.jsx
@@ -1,5 +1,76 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const SuccessContainer = styled.div`
-    padding-top: 67px;
+  padding-top: 67px;
+  h3 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.04em;
+    color: #293845;
+  }
+  h4 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22px;
+    line-height: 26px;
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.04em;
+    color: #293845;
+  }
+  button {
+    display: block;
+    margin: 0 auto;
+    width: 225px;
+    height: 42px;
+    background-color: #e8833a;
+    border-radius: 3px;
+    color: #fff;
+    font-family: "Roboto";
+    font-size: 18px;
+  }
+`;
+
+export const Headline = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 110px;
+  h2 {
+    text-align: center;
+    font-weight: 700;
+    font-size: var(--fsize5);
+    color: #247a6b;
+  }
+`;
+
+export const Campos = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 28px;
+  gap: 46px;
+  margin-bottom: 50px;
+`;
+
+export const CamposBox = styled.div``;
+
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
 `;
